2b980d1d4dc2458bca0a524c422a7460d0cbfb96
thetnaing <kothetnaingtun.ict@gmaiil.com>
2023-09-21 13:30:42 +0630

    Ajax Add and List

--- ajax_todo/myapp/urls.py
+++ ajax_todo/myapp/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('list', views.list, name='list'),
+    path('api/', views.TaskList.as_view(), name='TaskList'),
+    path('api/<int:pk>/', views.TaskDetail.as_view(), name='TaskDetail'),
 ]
--- ajax_todo/myapp/views.py
+++ ajax_todo/myapp/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from .models import *
+from .serializers import *
+
+from rest_framework import generics
+
+class TaskList(generics.ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
 
 # Create your views here.
 def list(request):
     return render(request, 'list.html')
+
